6d5184901738e9cebca2ece34db0643504d101df
abhinav-mara <107329487+abhinav-mara@users.noreply.github.com>
2022-07-22 14:52:26 +0000

    second commit
    done with naming jpgs in proper format (includes executable and raw image)

--- psets/pset4/recover/recover.c
+++ psets/pset4/recover/recover.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <stdint.h>
+
+int BLOCK_SIZE = 512;
+
 int main(int argc, char *argv[])
 {
- 
+	if (argc != 2) {
+		printf("Usage: ./recover IMAGE\n");
+		return 1;
+	}
+	
+	FILE* image = fopen(argv[1], "r");
+	uint8_t* buffer = malloc(BLOCK_SIZE);
+	
+	int ones = 0;
+	int tens = 0;
+	int hundreds = 0;
+	
+	char* name = malloc(sizeof(char) * 8);
+	sprintf(name, "%i%i%i.jpg", hundreds, tens, ones);
+	
+	while (fread(buffer, 1, BLOCK_SIZE, image) == BLOCK_SIZE) {
+		if (buffer[0] == 255 && buffer[1] == 216 && buffer[2] == 255 && (buffer[4] > 223 || buffer[4] < 240)) {
+			if (ones < 9) {
+				ones++;
+			}
+			else if (tens < 9) {
+				tens++;
+				ones = 0;
+			}
+			else {
+				hundreds++;
+				tens = 0;
+				ones = 0;
+			}
+			sprintf(name, "%i%i%i.jpg", hundreds, tens, ones);
+			
+		}
+	}
+	
+	free(buffer);
+	free(name);
+	
+	
 }

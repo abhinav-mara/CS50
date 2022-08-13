0dbf44b8a176efe143833e680115915ce59883bf
abhinav-mara <107329487+abhinav-mara@users.noreply.github.com>
2022-08-13 01:10:56 +0000

    final commit

--- misc/hashing/hash.c
+++ misc/hashing/hash.c
@@ -3,59 +3,73 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-int LONGEST_WORD = 100;
-
 typedef struct node {
 	char* word;
 	struct node* next;
 } node;
 
+int hash_func(char* word);
+
 int main(void) {
 	node* hash_table[26];
+	int index;
+	node* n;
 
-	node* n = malloc(sizeof(node));
-	n->word = "Abhinav";
-	n->next = NULL;
-	hash_table[0] = n;
+	int values[26];
+	for (int i = 0; i < 26; i++) {
+		values[i] = -1;
+	}
+	
+	char* name = "Abhinav";
 
+	index = hash_func(name);
+	values[index] = 0;
 	n = malloc(sizeof(node));
-	n->word = "Abhiram";
+	n->word = name; 
 	n->next = NULL;
-	hash_table[0]->next = n;
+	hash_table[index] = n;
 
+	name = "Mara";
+
+	index = hash_func(name);
+	values[index] = 0;
 	n = malloc(sizeof(node));
-	n->word = "Asad";
+	n->word = name;
 	n->next = NULL;
-	hash_table[0]->next->next = n;
+	hash_table[index] = n;
 
+	name = "Abhiram";
+	
+	index = hash_func(name);
+	values[index] = 0;
 	n = malloc(sizeof(node));
-	n->word = "Billa";
+	n->word = name;
 	n->next = NULL;
-	hash_table[1] = n;
+	hash_table[index]->next = n;
 
-	n = malloc(sizeof(node));
-	n->word = "Baadshah";
-	n->next = NULL;
-	hash_table[1]->next = n;
+	for (int i = 0; i < 26; i++) {
+		if (values[i] > -1) {
+			node* list = hash_table[i];
+			for (node* tmp = list; tmp != NULL; tmp = tmp->next) {
+				printf("%s\n", tmp->word);
+			}
+		}
+	}
 
-	n = malloc(sizeof(node));
-	n->word = "Ankitha";
-	n->next = NULL;
-	hash_table[0]->next->next->next = n;
-
-	for (int i = 0; i < 2; i++) {
-		node* list = hash_table[i];
-		for (node* tmp = list; tmp != NULL; tmp = tmp->next) {
-			printf("%s\n", tmp->word);
+	for (int i = 0; i < 26; i++) {
+		if (values[i] > -1) {
+			node* list = hash_table[i];
+			while (list != NULL) {
+				node* tmp = list->next;
+				free(list);
+				list = tmp;
+			}
 		}
 	}
 	
-	for (int i = 0; i < 2; i++) {
-		node* list = hash_table[i];
-		while (list != NULL) {
-			node* tmp = list->next;
-			free(list);
-			list = tmp;
-		}
-	}
+
+}
+
+int hash_func(char* word) {
+	return toupper(word[0]) - 65;
 }

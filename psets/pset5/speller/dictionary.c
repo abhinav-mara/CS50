40000a73841fca439be508890ec2a06e738a0cc5
abhinav-mara <107329487+abhinav-mara@users.noreply.github.com>
2022-09-21 11:23:33 -0400

    Memory bugs fixed
    - need to correct check()

--- psets/pset5/speller/dictionary.c
+++ psets/pset5/speller/dictionary.c
@@ -20,32 +20,120 @@ node;
 const unsigned int N = 26;
 
 // Hash table
-node *table[N];
+node *table[N+1];
+
+// Record table
+int record[N];
+
+// number of times hash() was run
+int hashCounter = 0;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
     // TODO
-    for (int i = 0; i < N; i++) {
-            node* n = malloc(sizeof(n));
-            n = table[i];
-            if (strcasecmp(n->word, word) == 0) {
-                    return true;
-            }
-            while (n->next != NULL) {
-                    if (strcasecmp(n->next->word, word) == 0) {
-                            return true;
-                    }
-                    n = n->next;
-            }
+    int index = hash(word);
+
+    for (node* tmp = table[index]; tmp != NULL; tmp = tmp->next) {
+        if (strcasecmp(tmp->word, word) - 10 == 0) {
+            return true;
+        }
     }
+
     return false;
 }
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    hashCounter++;
+    char letter = tolower(word[0]);
+    int index = 0;
+    switch (letter) {
+        case 'a':
+            index = 0;
+            break;
+        case 'b':
+            index = 1;
+            break;
+        case 'c':
+            index = 2;
+            break;
+        case 'd':
+            index = 3;
+            break;
+        case 'e':
+            index = 4;
+            break;
+        case 'f':
+            index = 5;
+            break;
+        case 'g':
+            index = 6;
+            break;
+        case 'h':
+            index = 7;
+            break;
+        case 'i':
+            index = 8;
+            break;
+        case 'j':
+            index = 9;
+            break;
+        case 'k':
+            index = 10;
+            break;
+        case 'l':
+            index = 11;
+            break;
+        case 'm':
+            index = 12;
+            break;
+        case 'n':
+            index = 13;
+            break;
+        case 'o':
+            index = 14;
+            break;
+        case 'p':
+            index = 15;
+            break;
+        case 'q':
+            index = 16;
+            break;
+        case 'r':
+            index = 17;
+            break;
+        case 's':
+            index = 18;
+            break;
+        case 't':
+            index = 19;
+            break;
+        case 'u':
+            index = 20;
+            break;
+        case 'v':
+            index = 21;
+            break;
+        case 'w':
+            index = 22;
+            break;
+        case 'x':
+            index = 23;
+            break;
+        case 'y':
+            index = 24;
+            break;
+        case 'z':
+            index = 25;
+            break;
+        default:
+            index = -1;
+            break;
+    }
+
+    return index;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
@@ -56,24 +144,44 @@ bool load(const char *dictionary)
 
     for (int i = 0; i < N; i++) {
         table[i] = NULL;
+        record[i] = -1;
     }
-    
+    node* n;
     while (!feof(fp)) {
-        node* n = malloc(sizeof(node));
+        bool skip = false;
+        n = malloc(sizeof(node));
         if (fgets(n->word, LENGTH + 1, fp) != NULL) {
             int index = hash(n->word);
-            node* end = table[index];
-            if (end == NULL) {
-                n->next = NULL;
-                table[index] = n;
+            if (index == -1) {
+               skip = true;
+               free(n);
             }
-            else {
-                while (end->next != NULL) {
-                    end = end->next;
+            if (!skip) {
+                record[index]++; 
+                node* end = table[index];
+                if (end == NULL) {
+                    n->next = NULL;
+                    table[index] = n;
                 }
-                n->next = NULL;
-                end->next = n;
+                else {
+                    /*
+                    node* tmp;
+                    for (tmp = end; tmp->next != NULL; tmp = tmp->next) {
+                        continue;
+                    }
+                    n->next = NULL;
+                    tmp->next = n;
+                    */
+                    while (end->next != NULL) {
+                        end = end->next;
+                    }
+                    n->next = NULL;
+                    end->next = n;
+                }
             }
+        }
+        else {
+            free(n);
         }
     }
     
@@ -89,9 +197,8 @@ unsigned int size(void)
     int wordCounter = 0;
     for (int i = 0; i < N; i++) {
         node* current = table[i];
-        while (current->next != NULL) {
+        for (node* tmp = current; current != NULL; current = current->next) {
             wordCounter++;
-            current = current->next;
         }
     }
     return wordCounter;
@@ -102,11 +209,16 @@ bool unload(void)
 {
     // TODO
     for (int i = 0; i < N; i++) {
-        node* list = table[i];
-        while (list->next != NULL) {
-            node* tmp = list->next;
-            free(list);
-            list = tmp;
+        while (table[i] != NULL) {
+            node* tmp = table[i]->next;
+            free(table[i]);
+            table[i] = tmp;
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (table[i] != NULL) {
+            return false;
         }
     }
     return true;
